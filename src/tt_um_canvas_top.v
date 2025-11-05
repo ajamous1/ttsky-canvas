8f316611599f798183c6bc99682c3fb89ccafdc9
Ahmad Jamous <114758348+ajamous1@users.noreply.github.com>
2025-11-05 15:16:53 -0500

    Rename io_in and io_out to ui_in and uo_out

--- src/tt_um_canvas_top.v
+++ src/tt_um_canvas_top.v
@@ -3,8 +3,8 @@
 
 module tt_um_canvas_top (
     // TT user IOs
-    input  wire [7:0] io_in,     // in[7:0]  from MCU
-    output wire [7:0] io_out,    // out[7:0] to baseboard (unused -> 0)
+    input  wire [7:0] ui_in,     // in[7:0]  from MCU
+    output wire [7:0] uo_out,    // out[7:0] to baseboard (unused -> 0)
 
     // TT user bidir IOs (we use as inputs only for I2C slave)
     input  wire [7:0] uio_in,    // uio[7:0] inputs
@@ -24,20 +24,20 @@ module tt_um_canvas_top (
     wire scl = uio_in[3];
     wire sda = uio_in[2];
 
-    //   io_in[6:3] = pushbuttons (active-low, momentary)
-    //   io_in[2:0] = RGB switches (level)
-    //   io_in[7]   = Brush/Eraser switch (level)
+    //   ui_in[6:3] = pushbuttons (active-low, momentary)
+    //   ui_in[2:0] = RGB switches (level)
+    //   ui_in[7]   = Brush/Eraser switch (level)
     // Buttons (invert because active-low)
-    wire btn_up    = ~io_in[6];
-    wire btn_down  = ~io_in[5];
-    wire btn_right = ~io_in[4];
-    wire btn_left  = ~io_in[3];
+    wire btn_up    = ~ui_in[6];
+    wire btn_down  = ~ui_in[5];
+    wire btn_right = ~ui_in[4];
+    wire btn_left  = ~ui_in[3];
 
     // switches
-    wire sw_red    = io_in[2];
-    wire sw_green  = io_in[1];
-    wire sw_blue   = io_in[0];
-    wire sw_brush  = io_in[7];
+    wire sw_red    = ui_in[2];
+    wire sw_green  = ui_in[1];
+    wire sw_blue   = ui_in[0];
+    wire sw_brush  = ui_in[7];
 
     //aliasing 
     //buttons[3] = btn_up, buttons[2] = btn_down, ...
@@ -48,9 +48,9 @@ module tt_um_canvas_top (
     // Tie off unused TT outputs/bidirs
     // -----------------------------
     wire [7:0] status_w;
-    assign io_out  = status_w;   // or expose debug/status if desired
-    assign uio_out = 8'b0;   // we never drive the bidir bus
-    assign uio_oe  = 8'b0;   // keep as inputs (open-drain bus handled externally)
+    assign uo_out  = status_w;   // or expose debug/status if desired
+    assign uio_out = 8'b0;       // we never drive the bidir bus
+    assign uio_oe  = 8'b0;       // keep as inputs (open-drain bus handled externally)
 
      tt_um_canvas u_project (
         .clk     (clk),
